e799fbc9e2eb8794579790469721a6ae03bd58e7
fnsne <watashiwafnsne@gmail.com>
2019-06-19 13:11:03 +0800

    extract getCurrentDate

--- static/js/note.js
+++ static/js/note.js
@@ -1,9 +1,14 @@
 $(window).bind("load", function () {
-    var today = new Date();
-    var d = [];
-    d.push(today.getFullYear()) ;
-    d.push(String(today.getMonth()+1).padStart(2, '0'));
-    d.push(String(today.getDate()).padStart(2, '0'));
-    today = d.join("-");
+    var today = getCurrentDate();
     $('#noteDate').html(today)
 });
+
+function getCurrentDate() {
+    var today = new Date();
+    var d = [];
+    d.push(today.getFullYear());
+    d.push(String(today.getMonth() + 1).padStart(2, '0'));
+    d.push(String(today.getDate()).padStart(2, '0'));
+    today = d.join("-");
+    return today;
+}
