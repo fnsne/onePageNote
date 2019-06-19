4d5f4bb3fab498f62259551f5f336198b61212d2
fnsne <watashiwafnsne@gmail.com>
2019-06-19 16:26:42 +0800

    add button to change 格數.

--- static/js/note.js
+++ static/js/note.js
@@ -1,16 +1,70 @@
 $(window).bind("load", function () {
-    var today = getCurrentDate();
-    $('#noteDate').html(today)
+    $('#button8').click(function () {
+        genGrids(8)
+    });
 
-    var num = 7;
-    for (i= 0; i < num;i++){
-        createBaseGrid();
-    }
+    $('#button16').click(function () {
+        genGrids(16)
+    });
+
+    $('#button32').click(function () {
+        genGrids(32)
+    });
+
+    $('#button64').click(function () {
+        genGrids(64)
+    });
+
+    var today = getCurrentDate();
+    $('#noteDate').html(today);
+
+    var number = 8;
+
+    genGrids(number);
 });
 
-function createBaseGrid() {
-    var template = $('#baseGridTemplate').html();
-    $('#parent').append(template)
+
+function genGrids(number) {
+    $('.baseGrid').remove();
+    switch (number) {
+        case 8:
+            x = 4;
+            y = 2;
+            break;
+        case 16:
+            x = 4;
+            y = 4;
+            break;
+        case 32:
+            x = 8;
+            y = 4;
+            break
+        case 64:
+            x = 8;
+            y = 8;
+            break;
+
+    }
+
+
+    columnWidth = Math.floor(100 / x);
+    console.log("columnWidth = ", columnWidth);
+    columnTmp = "repeat(" + x.toString() + "," + columnWidth.toString() + "%)";
+    console.log("columnTmp = ", columnTmp);
+
+    rowHeight = Math.floor(100 / y);
+    console.log("rowHeight = ", rowHeight);
+    rowTmp = "repeat(" + y.toString() + "," + rowHeight.toString() + "%)";
+    console.log("rowTmp = ", rowTmp);
+
+    parent = $('#parent');
+    parent.css("grid-template-columns", columnTmp);
+    parent.css("grid-template-rows", rowTmp);
+    var num = number - 1;
+    for (i = 0; i < num; i++) {
+        var template = $('#baseGridTemplate').html();
+        parent.append(template)
+    }
 }
 
 function getCurrentDate() {
