4ebe72afb6feded8c676db02b8c8cb00f9d49714
fnsne <watashiwafnsne@gmail.com>
2020-01-17 15:59:56 +0800

    add decorator to check input id

--- server.go
+++ server.go
@@ -53,61 +53,27 @@ func (s *OnePageNoteServer) homePage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s *OnePageNoteServer) createNote(w http.ResponseWriter, r *http.Request) {
+func (s *OnePageNoteServer) updateNote(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(getNoteId(r))
 	var note Note
 	err := json.NewDecoder(r.Body).Decode(&note)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	id := s.store.CreateNote(note)
-	w.WriteHeader(http.StatusOK)
-	note.Id = id
-	err = json.NewEncoder(w).Encode(&note)
-	if err != nil {
-		fmt.Println("err in post to create note return id", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	return
-}
-
-func (s *OnePageNoteServer) updateNote(w http.ResponseWriter, r *http.Request) {
-	idString := r.URL.Path[len("/api/note/"):]
-	id, err := strconv.Atoi(idString)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Println("err = ", err)
-		return
-	}
-	var note Note
-	err = json.NewDecoder(r.Body).Decode(&note)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
 		var p []byte
 		r.Body.Read(p)
-		fmt.Println("r.Body = ", p)
-		fmt.Println("err = ", err)
 	} else {
 		if len(s.store.GetNoteList()) == 0 {
 			s.store.CreateNote(note)
 		}
 		s.store.SetNote(id, note)
-		fmt.Println("set note ", id, "to ", note)
 		w.WriteHeader(http.StatusOK)
 	}
 	return
 }
 
 func (s *OnePageNoteServer) retrieveNote(w http.ResponseWriter, r *http.Request) {
-	idString := r.URL.Path[len("/api/note/"):]
-	id, err := strconv.Atoi(idString)
-	if err != nil {
-		fmt.Println("err= ", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	err = json.NewEncoder(w).Encode(s.store.GetNote(id))
+	id, _ := strconv.Atoi(getNoteId(r))
+	err := json.NewEncoder(w).Encode(s.store.GetNote(id))
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -128,13 +94,32 @@ func (s *OnePageNoteServer) listNote(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+func (s *OnePageNoteServer) createNote(w http.ResponseWriter, r *http.Request) {
+	var note Note
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	id := s.store.CreateNote(note)
+	w.WriteHeader(http.StatusOK)
+	note.Id = id
+	err = json.NewEncoder(w).Encode(&note)
+	if err != nil {
+		fmt.Println("err in post to create note return id", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	return
+}
+
 func (s *OnePageNoteServer) note(w http.ResponseWriter, r *http.Request) {
 	if requestHasId(r) {
 		if r.Method == http.MethodGet {
-			s.retrieveNote(w, r)
+			confirmId(s.retrieveNote)(w, r)
 		}
 		if r.Method == http.MethodPost {
-			s.updateNote(w, r)
+			confirmId(s.updateNote)(w, r)
 		}
 	} else {
 		if r.Method == http.MethodGet {
@@ -147,10 +132,29 @@ func (s *OnePageNoteServer) note(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusBadRequest)
 }
 
+func confirmId(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idString := getNoteId(r)
+		_, err := strconv.Atoi(idString)
+		if err != nil {
+			fmt.Println("err= ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		} else {
+			f(w, r)
+		}
+	}
+}
+
 func requestHasId(r *http.Request) bool {
-	idString := r.URL.Path[len("/api/note/"):]
+	idString := getNoteId(r)
 	hasId := len(idString) != 0
 	return hasId
+}
+
+func getNoteId(r *http.Request) string {
+	idString := r.URL.Path[len("/api/note/"):]
+	return idString
 }
 
 type Grid struct {
